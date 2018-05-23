77ba667801f9348f103122cd81f0511e6a29edb9
unknown <hao.yu@careate.cn>
2018-05-23 15:19:19 +0800

    test commit

---
+++ src/cmd/helloworld/HelloWorldServer/server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "<h1>Hello World!</h1>")
+	})
+	http.ListenAndServe(":8888", nil)
+}
