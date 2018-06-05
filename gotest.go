6bcbde3c4ba0870743f063d29d2e151e0e522671
yuhao <19614102@qq.com>
2018-06-05 13:52:37 +0800

    测试提交

--- gotest.go
+++ gotest.go
@@ -6,4 +6,15 @@ import (
 
 func main() {
 	fmt.Println(2123123)
+	people := People{Name: "yuhao", Age: 24}
+	fmt.Println(people)
+	alter(people)
+	fmt.Println(people)
+	var a Fee
+	a = &people
+	a.Feed()
+	tttt(a)
+}
+func alter(people People) {
+	people.Name = "Lili"
 }
--- test1.go
+++ test1.go
@@ -1,5 +1,26 @@
 package main
 
-func test() {
+import (
+	"fmt"
+)
+
+type People struct {
+	Name string
+	Age  int
+}
+
+func (people *People) Feed() {
+	fmt.Println("接口测试啊啊啊啊")
+}
+
+type Fee interface {
+	Feed()
+}
+
+func tttt(fee Fee) {
+	fee.Feed()
+}
+
+func (people *People) test() {
 
 }
