ad21d9a6db89ed393188988b6df5c49309a76e18
yuhao <19614102@qq.com>
2018-06-07 16:51:42 +0800

    2132342

--- gotest.go
+++ gotest.go
@@ -4,17 +4,57 @@ import (
 	"fmt"
 )
 
+type Foo struct {
+	Name string
+	Age  int
+}
+
+func (self *Foo) Get() {
+	self.Name = "测试"
+	return
+}
+
+func (self Foo) GetConst() {
+	self.Name = "测试"
+	return
+}
 func main() {
 	fmt.Println(2123123)
 	people := People{Name: "yuhao", Age: 24}
+	foo := Foo{Name: "校长", Age: 12}
 	fmt.Println(people)
 	alter(people)
 	fmt.Println(people)
 	var a Fee
 	a = &people
+	fmt.Printf("%p", &people)
+	fmt.Println()
+	people.Feed()
+	fmt.Println()
 	a.Feed()
 	tttt(a)
+	fmt.Println(foo)
+	foo.Get()
+	fmt.Println(foo)
 }
 func alter(people People) {
 	people.Name = "Lili"
+}
+
+type People struct {
+	Name string
+	Age  int
+}
+
+func (people *People) Feed() {
+	fmt.Printf("接口测试啊啊啊啊 %p", &people)
+	fmt.Println()
+}
+
+type Fee interface {
+	Feed()
+}
+
+func tttt(fee Fee) {
+	fee.Feed()
 }
