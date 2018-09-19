231a97cba16f0c98df25c71d1522c5eb4c5d770f
yuhao <19614102@qq.com>
2018-09-19 17:39:26 +0800

    monitor

---
+++ CareateMonitor/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "GOTests/CareateMonitor/monitor"
+
+func main() {
+	a := monitor.InitMonitor()
+	go a.MonitorLoop()
+	a.Write()
+	// if session, err := mgo.Dial("mongodb://CareateAdmin:Careate2016!@127.0.0.1:27017/CareateMonitorDB"); err != nil {
+	// 	fmt.Println(err.Error())
+	// 	fmt.Println(1)
+	// } else {
+	// 	fmt.Println(session)
+	// 	fmt.Println(1)
+	// }
+
+	// m, _ := mem.VirtualMemory()
+	// d, _ := disk.Usage("/")
+	//n, _ := net.Connections("tcp")
+
+	// established := 0
+	// timewait := 0
+	// closewait := 0
+	// for _, item := range n {
+	// 	if item.Status == "ESTABLISHED" {
+	// 		established++
+	// 	}
+	// 	if item.Status == "TIME_WAIT" {
+	// 		timewait++
+	// 	}
+	// 	if item.Status == "CLOSE_WAIT" {
+	// 		closewait++
+	// 	}
+	// }
+	// almost every return value is a struct
+	// fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", m.Total, m.Free, m.UsedPercent)
+
+	// // convert to JSON. String() is also implemented
+	// fmt.Println(d)
+	// fmt.Println(n)
+	// fmt.Println(established)
+	// fmt.Println(timewait)
+	// fmt.Println(closewait)
+}
---
+++ CareateMonitor/monitor/monitor.go
@@ -0,0 +1,200 @@
+package monitor
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
+	gopsutilnet "github.com/shirou/gopsutil/net"
+)
+
+//Monitor ：监控主体
+type Monitor struct {
+	IP            string
+	ServerName    string
+	messageChanel chan *MonitorMessage
+}
+
+func (monitor *Monitor) Write() {
+	for {
+		data := <-monitor.messageChanel
+		fmt.Println(data)
+	}
+}
+
+//InitMonitor ： 初始化
+func InitMonitor() (monitor *Monitor) {
+	var addr string
+	if addrs, err := net.InterfaceAddrs(); err == nil {
+		for _, address := range addrs {
+
+			// 检查ip地址判断是否回环地址
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					addr = ipnet.IP.String()
+					break
+				}
+			}
+		}
+	}
+	monitor = &Monitor{
+		messageChanel: make(chan *MonitorMessage),
+		IP:            addr,
+		ServerName:    "测试serverName",
+	}
+	return
+}
+
+//MonitorLoop : 循环监控 间隔X秒
+func (monitor *Monitor) MonitorLoop() {
+	for {
+		go monitor.getCPUStat()
+		go monitor.getDiskStat()
+		go monitor.getMemStat()
+		go monitor.getNetStat()
+		time.Sleep(5 * time.Second)
+	}
+}
+
+//getMemStat : 循环获取内存状态
+func (monitor *Monitor) getMemStat() {
+	if m, err := mem.VirtualMemory(); err == nil {
+		monitorMessage := &MonitorMessage{
+			Key:         "RAM",
+			Value:       float64ToString(m.UsedPercent),
+			Description: "内存",
+			InDateTime:  getTime(),
+			ServerName:  monitor.ServerName,
+			ServerIP:    monitor.IP,
+		}
+		monitor.messageChanel <- monitorMessage
+	}
+}
+
+//getDiskStat : 循环获取内存状态
+func (monitor *Monitor) getDiskStat() {
+	if d, err := disk.Usage("/"); err == nil {
+		monitorMessage := &MonitorMessage{
+			Key:         "DISK",
+			Value:       float64ToString(d.UsedPercent),
+			Description: "硬盘",
+			InDateTime:  getTime(),
+			ServerName:  monitor.ServerName,
+			ServerIP:    monitor.IP,
+		}
+		monitor.messageChanel <- monitorMessage
+	}
+}
+
+//getCPUStat : 循环获取CPU状态
+func (monitor *Monitor) getCPUStat() {
+	if cp, err := cpu.Percent(time.Second, false); err == nil {
+		monitorMessage := &MonitorMessage{
+			Key:         "CPU",
+			Value:       float64ToString(cp[0]),
+			Description: "CPU",
+			InDateTime:  getTime(),
+			ServerName:  monitor.ServerName,
+			ServerIP:    monitor.IP,
+		}
+		monitor.messageChanel <- monitorMessage
+	}
+}
+
+func (monitor *Monitor) getNetStat() {
+	n, _ := gopsutilnet.Connections("tcp")
+	established := 0
+	timewait := 0
+	closewait := 0
+	redis := false
+	elasticsearch := false
+	for _, item := range n {
+		if item.Laddr.Port == 6379 && item.Status == "LISTEN" {
+			redis = true
+		}
+		if item.Laddr.Port == 9200 && item.Status == "LISTEN" {
+			elasticsearch = true
+		}
+		if item.Status == "ESTABLISHED" {
+			established++
+		}
+		if item.Status == "TIME_WAIT" {
+			timewait++
+		}
+		if item.Status == "CLOSE_WAIT" {
+			closewait++
+		}
+	}
+	go monitor.getRedisStat(redis)
+	go monitor.getElasticsearchStat(elasticsearch)
+	go monitor.getStat("ESTABLISHED", strconv.Itoa(established))
+	go monitor.getStat("TIME_WAIT", strconv.Itoa(timewait))
+	go monitor.getStat("CLOSE_WAIT", strconv.Itoa(closewait))
+}
+func (monitor *Monitor) getStat(key string, value string) {
+	monitorMessage := &MonitorMessage{
+		Key:         key,
+		Value:       value,
+		Description: key,
+		InDateTime:  getTime(),
+		ServerName:  monitor.ServerName,
+		ServerIP:    monitor.IP,
+	}
+	monitor.messageChanel <- monitorMessage
+}
+func (monitor *Monitor) getRedisStat(n bool) {
+	redisMonitorMessage := &MonitorMessage{
+		Key:         "Service_redis",
+		Value:       "close",
+		Description: "redis服务",
+		InDateTime:  getTime(),
+		ServerName:  monitor.ServerName,
+		ServerIP:    monitor.IP,
+	}
+	if n {
+		redisMonitorMessage.Value = "open"
+	}
+	monitor.messageChanel <- redisMonitorMessage
+}
+
+func (monitor *Monitor) getElasticsearchStat(n bool) {
+	monitorMessage := &MonitorMessage{
+		Key:         "Service_elasticsearch",
+		Value:       "close",
+		Description: "elasticsearch服务",
+		InDateTime:  getTime(),
+		ServerName:  monitor.ServerName,
+		ServerIP:    monitor.IP,
+	}
+	if n {
+		monitorMessage.Value = "open"
+	}
+	monitor.messageChanel <- monitorMessage
+}
+
+//GetTime : 获取当前时间字符串
+func getTime() (dataTimeStr string) {
+	dataTimeStr = time.Now().Format("2006-01-02 15:04:05")
+	return
+}
+
+// float64ToString : float64转 string
+func float64ToString(f float64) (str string) {
+	str = strconv.FormatFloat(f, 'f', 2, 64) + "%"
+	return
+}
+
+//MonitorMessage : 监控消息
+type MonitorMessage struct {
+	ServerName  string
+	ServerIP    string
+	InDateTime  string
+	Key         string
+	Value       string
+	Description string
+	CreateTime  string
+}
